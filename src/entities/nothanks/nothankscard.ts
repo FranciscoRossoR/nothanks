63fdf61efeba63aca9a2be22e2d812bc7c2d6d2e
FranciscoRossoR <franrosora@gmail.com>
2025-05-25 20:47:41 +0200

    fix: add uid to Card

--- src/entities/nothanks/nothankscard.ts
+++ src/entities/nothanks/nothankscard.ts
@@ -2,8 +2,8 @@ import Card from "../framework/card"
 
 export class NoThanksCard extends Card {
     readonly value: number;
-    public constructor (value: number) {
-        super (value.toString());
+    public constructor (value: number, uid?: string) {
+        super (value.toString(), uid);
         this.value = -value;
     }
 
